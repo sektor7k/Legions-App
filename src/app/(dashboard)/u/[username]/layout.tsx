92bcff4fc50fb17560c955918cb70051e0b15848
sektor7k <omeraydin2112@gmail.com>
2024-07-23 04:41:05 +0300

    dashboard layout created

---
+++ src/app/(dashboard)/u/[username]/layout.tsx
@@ -0,0 +1,19 @@
+import Header from '@/components/layout/header';
+import Sidebar from '@/components/layout/sidebar';
+
+
+export default function DashboardLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 overflow-hidden pt-16">{children}</main>
+      </div>
+    </>
+  );
+}
