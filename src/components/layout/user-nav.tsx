e0d9f32c3c736c24685c9101a0c997a2e22ab62e
sektor7k <omeraydin2112@gmail.com>
2024-07-23 04:43:50 +0300

    use-nav

---
+++ src/components/layout/user-nav.tsx
@@ -0,0 +1,96 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem, 
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/components/ui/use-toast';
+import AvatarDemo from './avatarDemo';
+export function UserNav() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+    const {toast} = useToast();
+
+    const logout = async () => {
+        try {
+            signOut()
+            router.push("/login") 
+            showToast(":)")
+
+        } catch (error: any) {
+            console.log(error.data.message);
+            showErrorToast(error.data.message)
+
+        }
+    }
+
+    function showErrorToast(message: string): void {
+        toast({
+            variant: "destructive",
+            title: "Logout failed",
+            description: message,
+        })
+    }
+
+    function showToast(message: string): void {
+        toast({
+            variant: "default",
+            title: "Logout Succesfuly",
+            description: message,
+        })
+    }
+
+  if (session) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <AvatarDemo classname={'w-8 h-8'} img={'https://github.com/sektor7k.png'} username={session.user.username} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session.user?.username}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {session.user?.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={()=>{ router.push(`/u/${session.user.username}`)}}>
+              Profile
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Billing
+              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Settings
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>New Team</DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={logout}>
+            Log out
+            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
+}
