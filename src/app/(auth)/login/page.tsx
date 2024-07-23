481bc9972a8b720f32cb686ebb74c4106ba0b332
sektor7k <omeraydin2112@gmail.com>
2024-07-23 04:40:19 +0300

    add logo

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
             if (response?.error) {
                 showErrorToast(response.error);
             } else {
-                showToast("Succesfuly");
+                showToast("Welcome");
             }
         } catch (error: any) {
             console.log(error)
