bdb8eeefba35c023ed9e2b84a14556d0da7b143e
sektor7k <omeraydin2112@gmail.com>
2024-07-25 03:27:24 +0300

    profile adresses moved

--- src/app/(dashboard)/u/[username]/profile/_components/profile-adresses.tsx
+++ src/app/(dashboard)/u/[username]/profile/_components/profile-adresses.tsx
@@ -1,7 +1,241 @@
-export default function ProfileAdresses(){
-    return(
-        <div>
-            
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { Label } from "@/components/ui/label";
+
+export default function ProfileAdresses() {
+    return (
+        <div className="space-y-6 lg:space-y-10">
+            <div className="flex flex-col space-y-2 lg:space-y-4">
+                <div className="flex items-center space-x-2">
+                    <h2 className="scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0">
+                        SUB-WALLET ADDRESS
+                    </h2>
+                </div>
+            </div>
+
+
+            <div className="flex flex-col items-center space-y-6">
+                <div className="flex flex-col">
+                    <p className="text-lg text-muted-foreground mb-3">EVM Adress</p>
+                    <div className="flex items-center ">
+                        <img src="https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=032" alt="" className="w-4 mr-2" />
+                        <Input type="text" placeholder="Twitter" disabled value={"0xd40F6f3CeCb2b601744019b5aBEDb78809327011"} className="rounded-r-none w-96" />
+                        <Sheet >
+                            <SheetTrigger>
+                                <Button variant={"secondary"} className=" rounded-r-3xl rounded-l-none ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side={"bottom"}>
+                                <SheetHeader>
+                                    <SheetTitle>EVM Adress</SheetTitle>
+                                    <SheetDescription>
+                                        Please enter your EVM wallet address.
+                                    </SheetDescription>
+                                </SheetHeader>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Adress
+                                    </Label>
+                                    <Input id="name" placeholder="0xd40F6*************************************" className="col-span-3" />
+                                </div>
+                                <SheetFooter>
+                                    <SheetClose asChild>
+                                        <Button type="submit">Save changes</Button>
+                                    </SheetClose>
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-lg text-muted-foreground mb-3">BTC Adress</p>
+                    <div className="flex items-center ">
+                        <img src="https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=032" alt="" className="w-4 mr-2" />
+                        <Input type="text" placeholder="Twitter" disabled value={"0xd40F6f3CeCb2b601744019b5aBEDb78809327011"} className="rounded-r-none w-96" />
+                        <Sheet >
+                            <SheetTrigger>
+                                <Button variant={"secondary"} className=" rounded-r-3xl rounded-l-none ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side={"bottom"}>
+                                <SheetHeader>
+                                    <SheetTitle>BTC Adress</SheetTitle>
+                                    <SheetDescription>
+                                        Please enter your BTC wallet address.
+                                    </SheetDescription>
+                                </SheetHeader>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Adress
+                                    </Label>
+                                    <Input id="name" placeholder="0xd40F6*************************************" className="col-span-3" />
+                                </div>
+                                <SheetFooter>
+                                    <SheetClose asChild>
+                                        <Button type="submit">Save changes</Button>
+                                    </SheetClose>
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-lg text-muted-foreground mb-3">Solana Adress</p>
+                    <div className="flex items-center ">
+                        <img src="https://cryptologos.cc/logos/solana-sol-logo.svg?v=032" alt="" className="w-4 mr-2" />
+                        <Input type="text" placeholder="Twitter" disabled value={"0xd40F6f3CeCb2b601744019b5aBEDb78809327011"} className="rounded-r-none w-96" />
+                        <Sheet >
+                            <SheetTrigger>
+                                <Button variant={"secondary"} className=" rounded-r-3xl rounded-l-none ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side={"bottom"}>
+                                <SheetHeader>
+                                    <SheetTitle>SOL Adress</SheetTitle>
+                                    <SheetDescription>
+                                        Please enter your Solana wallet address.
+                                    </SheetDescription>
+                                </SheetHeader>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Adress
+                                    </Label>
+                                    <Input id="name" placeholder="0xd40F6*************************************" className="col-span-3" />
+                                </div>
+                                <SheetFooter>
+                                    <SheetClose asChild>
+                                        <Button type="submit">Save changes</Button>
+                                    </SheetClose>
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-lg text-muted-foreground mb-3">Sei Adress</p>
+                    <div className="flex items-center ">
+                        <img src="https://cryptologos.cc/logos/sei-sei-logo.svg?v=032" alt="" className="w-4 mr-2" />
+                        <Input type="text" placeholder="Twitter" disabled value={"0xd40F6f3CeCb2b601744019b5aBEDb78809327011"} className="rounded-r-none w-96" />
+                        <Sheet >
+                            <SheetTrigger>
+                                <Button variant={"secondary"} className=" rounded-r-3xl rounded-l-none ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side={"bottom"}>
+                                <SheetHeader>
+                                    <SheetTitle>Sei Adress</SheetTitle>
+                                    <SheetDescription>
+                                        Please enter your Sei wallet address.
+                                    </SheetDescription>
+                                </SheetHeader>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Adress
+                                    </Label>
+                                    <Input id="name" placeholder="0xd40F6*************************************" className="col-span-3" />
+                                </div>
+                                <SheetFooter>
+                                    <SheetClose asChild>
+                                        <Button type="submit">Save changes</Button>
+                                    </SheetClose>
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-lg text-muted-foreground mb-3">Aptos Adress</p>
+                    <div className="flex items-center ">
+                        <img src="https://cryptologos.cc/logos/aptos-apt-logo.svg?v=032" alt="" className="w-4 mr-2" />
+                        <Input type="text" placeholder="Twitter" disabled value={"0xd40F6f3CeCb2b601744019b5aBEDb78809327011"} className="rounded-r-none w-96" />
+                        <Sheet >
+                            <SheetTrigger>
+                                <Button variant={"secondary"} className=" rounded-r-3xl rounded-l-none ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side={"bottom"}>
+                                <SheetHeader>
+                                    <SheetTitle>Aptos Adress</SheetTitle>
+                                    <SheetDescription>
+                                        Please enter your Aptos wallet address.
+                                    </SheetDescription>
+                                </SheetHeader>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Adress
+                                    </Label>
+                                    <Input id="name" placeholder="0xd40F6*************************************" className="col-span-3" />
+                                </div>
+                                <SheetFooter>
+                                    <SheetClose asChild>
+                                        <Button type="submit">Save changes</Button>
+                                    </SheetClose>
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-lg text-muted-foreground mb-3">Sui Adress</p>
+                    <div className="flex items-center ">
+                        <img src="https://cryptologos.cc/logos/sui-sui-logo.svg?v=032" alt="" className="w-4 mr-2" />
+                        <Input type="text" placeholder="Twitter" disabled value={"0xd40F6f3CeCb2b601744019b5aBEDb78809327011"} className="rounded-r-none w-96" />
+                        <Sheet >
+                            <SheetTrigger>
+                                <Button variant={"secondary"} className=" rounded-r-3xl rounded-l-none ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side={"bottom"}>
+                                <SheetHeader>
+                                    <SheetTitle>Sui Adress</SheetTitle>
+                                    <SheetDescription>
+                                        Please enter your Sui wallet address.
+                                    </SheetDescription>
+                                </SheetHeader>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Adress
+                                    </Label>
+                                    <Input id="name" placeholder="0xd40F6*************************************" className="col-span-3" />
+                                </div>
+                                <SheetFooter>
+                                    <SheetClose asChild>
+                                        <Button type="submit">Save changes</Button>
+                                    </SheetClose>
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
