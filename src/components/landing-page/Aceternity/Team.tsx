ba3b27b1f03b9af0230deb10c3280b0184031ecb
sektor7k <omeraydin2112@gmail.com>
2024-07-26 05:16:11 +0300

    team component

---
+++ src/components/landing-page/Aceternity/Team.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React from "react";
+import { PinContainer } from "@/components/ui/3d-pin";
+import AvatarDemo from "@/components/layout/avatarDemo";
+
+export function Team() {
+    return (
+
+        <section
+            id="team"
+            className="container py-24 sm:py-32"
+        >
+            <h2 className="text-3xl md:text-4xl font-bold">
+                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+                    OUR
+                </span>
+                TEAM
+            </h2>
+
+            <p className="mt-4 mb-10 text-xl text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+                dolor pariatur sit!
+            </p>
+
+            <div className="h-auto w-full flex flex-col items-center justify-center sm:flex-row sm:flex-wrap ">
+                <PinContainer title="github.com/sektor7k" href="https://github.com/sektor7k">
+                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/3 w-[20rem] h-[20rem] justify-center items-center">
+                        <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100">
+                            Sektor7K
+                        </h3>
+                        <div className="text-base !m-0 !p-0 font-normal">
+                            <span className="text-slate-500">Fullstack Developer</span>
+                        </div>
+                        <AvatarDemo
+                            classname="flex flex-1 w-3/4 rounded-full mt-4 "
+                            img="https://github.com/sektor7k.png"
+                            username="user"
+                        />
+                    </div>
+                </PinContainer>
+
+                <PinContainer title="x.com/BerkayFall" href="https://x.com/BerkayFall">
+                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/3 w-[20rem] h-[20rem] justify-center items-center">
+                        <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100">
+                            Fall
+                        </h3>
+                        <div className="text-base !m-0 !p-0 font-normal">
+                            <span className="text-slate-500">Desinger - Marketing</span>
+                        </div>
+                        <AvatarDemo
+                            classname="flex flex-1 w-3/4 rounded-full mt-4 bg-gradient-to-br"
+                            img="https://pbs.twimg.com/profile_images/1664014334337470467/DNxDdOua_400x400.jpg"
+                            username="user"
+                        />
+                    </div>
+                </PinContainer>
+
+                <PinContainer title="x.com/rabbitcoinhole" href="https://x.com/rabbitcoinhole">
+                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/3 w-[20rem] h-[20rem] justify-center items-center">
+                        <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base text-slate-100">
+                        Rabbitcoinhole
+                        </h3>
+                        <div className="text-base !m-0 !p-0 font-normal">
+                            <span className="text-slate-500">Influcer</span>
+                        </div>
+                        <AvatarDemo
+                            classname="flex flex-1 w-3/4 rounded-full mt-4 bg-gradient-to-br"
+                            img="https://pbs.twimg.com/profile_images/1755154072728940544/QSEWR0OS_400x400.jpg"
+                            username="user"
+                        />
+                    </div>
+                </PinContainer>
+            </div>
+
+
+
+        </section>
+    );
+}
