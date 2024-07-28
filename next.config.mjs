1aec70146ed9f72a76fe85cace0fa10d7a6a72a5
sektor7k <omeraydin2112@gmail.com>
2024-07-29 00:10:54 +0300

    vercel bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['utfs.io', "aceternity.com", "www.behance.net"], // İzin verilen ana makineler
+        domains: ['utfs.io', "aceternity.com", "www.behance.net","cryptologos.cc"], // İzin verilen ana makineler
       },
 };
 
--- src/app/(dashboard)/u/[username]/profile/_components/profile-user.tsx
+++ src/app/(dashboard)/u/[username]/profile/_components/profile-user.tsx
@@ -45,7 +45,7 @@ export default function ProfileUser() {
                             <DialogHeader>
                                 <DialogTitle>Edit profile</DialogTitle>
                                 <DialogDescription>
-                                    Make changes to your profile here. Click save when you're done.
+                                    Make changes to your profile here. Click save when you&apos;re done.
                                 </DialogDescription>
                             </DialogHeader>
                             <div className="flex flex-col">
