f6d0ebc5501dcb15a58c65ea2d7ae7f0276a642d
sektor7k <omeraydin2112@gmail.com>
2024-07-31 06:13:38 +0300

    register metamask

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,6 +4,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import {
+    IconBrandCashapp,
     IconBrandGithub,
     IconBrandGoogle,
     IconBrandOnlyfans,
@@ -15,7 +16,14 @@ import Link from "next/link";
 import { toast } from "@/components/ui/use-toast";
 import { Button } from "@/components/ui/button";
 import { signIn, useSession } from "next-auth/react";
- 
+import { ethers } from "ethers";
+
+declare global {
+    interface Window {
+        ethereum?: any;
+    }
+}
+
 export default function RegisterPage() {
 
     const router = useRouter();
@@ -29,9 +37,9 @@ export default function RegisterPage() {
 
     useEffect(() => {
         if (session) {
-          router.push("/");
+            router.push("/");
         }
-      }, [session, router]);
+    }, [session, router]);
 
     const onSignup = async () => {
 
@@ -67,6 +75,13 @@ export default function RegisterPage() {
             description: message,
         })
     }
+    function showToastWallet(message: string): void {
+        toast({
+            variant: "default",
+            title: "Please",
+            description: message,
+        })
+    }
 
     function showToast(message: string): void {
         toast({
@@ -74,6 +89,46 @@ export default function RegisterPage() {
             title: "Please Check email",
             description: message,
         })
+    }
+
+    async function registerMetamask() {
+        try {
+            if (!window.ethereum) {
+                window.alert("Please install MetaMask first.");
+                return;
+            }
+
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            const signer = await provider.getSigner();
+            const publicAddress = await signer.getAddress();
+            console.log("PUBLIC ADDRESS", publicAddress);
+
+            // Nonce oluşturma API'sine istek gönder
+            const response = await axios.post('/api/auth/crypto/generateNonce', { publicAddress });
+            console.log("RESPONSE", response.data);
+
+            if (!response.data.nonce) {
+                console.error("Failed to fetch nonce");
+                return;
+            }
+
+            // Nonce'u imzala
+            const signedNonce = await signer.signMessage(response.data.nonce);
+            console.log("SIGNED NONCE", signedNonce);
+
+            await axios.post('/api/auth/crypto/register', {
+                publicAddress,
+                signedNonce
+            });
+            router.push('/login')
+
+            showToastWallet("Signup successfully!");
+
+
+        } catch (error: any) {
+            const errorMessage = error.response.data.message;
+            showErrorToast(errorMessage);
+        }
     }
 
 
@@ -144,13 +199,49 @@ export default function RegisterPage() {
 
                 <div className="flex flex-col space-y-4">
                     <button
+                        onClick={registerMetamask}
+                        className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        type="submit"
+                    >
+                        <svg viewBox="0 0 64 64" className=" h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#f8f7f7">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0">
+                            </g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
+                            </g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path d="m54 26 2 2-4 4 4 12-2 10-12-4-6 4h-8l-6-4-12 4-2-10 4-12-4-4 2-2-2-10 2-8 14 8h16l14-8 2 8-2 10z">
+                                </path>
+                                <path d="m40 16-2 8-2 16h-8l-2-16-2-8">
+                                </path>
+                                <path d="m28 40-6 10">
+                                </path>
+                                <path d="m36 40 6 10">
+                                </path>
+                                <path d="M32 48v6">
+                                </path>
+                                <path d="m12 32 14-8">
+                                </path>
+                                <path d="m38 24 14 8">
+                                </path>
+                                <path d="m28 40-10-4">
+                                </path>
+                                <path d="m36 40 10-4">
+                                </path>
+                            </g>
+                        </svg>
+                        <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                            Metamask
+                        </span>
+                        <BottomGradient />
+                    </button>
+                    <button
                         onClick={() => signIn("github")}
                         className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
                         type="submit"
                     >
                         <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                         <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                            GitHub
+                            Github
                         </span>
                         <BottomGradient />
                     </button>
