03a00e02dc41ff07d6909d06f3ce488516967663
sektor7k <omeraydin2112@gmail.com>
2024-08-01 22:38:27 +0300

    coingecko route

--- src/app/(browse)/example/page.tsx
+++ src/app/(browse)/example/page.tsx
@@ -1,26 +1,30 @@
 "use client"
 
-import React from 'react'
-
-
-
-
-
+import React, { useEffect, useState } from 'react';
+import Lottie from 'react-lottie';
 
 export default function Page() {
+  const [animationData, setAnimationData] = useState(null);
 
-    
+  useEffect(() => {
+    fetch('https://lottie.host/f9922fbd-b1fe-41dd-af7e-87d4ddbe631e/oDCBA49pXD.json')
+      .then(response => response.json())
+      .then(data => setAnimationData(data));
+  }, []);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
-      
-      <div className="w-full h-96">
-        
+    <div>
+      {animationData && <Lottie options={defaultOptions} height={400} width={400} />}
       </div>
-    </div>
-  )
+    
+  );
 }
-
-
-
-
