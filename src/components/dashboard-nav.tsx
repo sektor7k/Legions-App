b8a81ff7c40b17571b1cba6189f3f9e3876fa69c
sektor7k <omeraydin2112@gmail.com>
2024-08-07 03:13:23 +0300

    add ittem dashboard nav

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -80,3 +80,63 @@ export function DashboardNav({
     </nav>
   );
 }
+
+export function DashboardNavHeader({
+  items,
+  setOpen,
+  isMobileNav = false
+}: DashboardNavProps) {
+  const path = usePathname();
+  const { isMinimized } = useSidebar();
+
+  if (!items?.length) {
+    return null;
+  }
+
+  console.log('isActive', isMobileNav, isMinimized);
+
+  return (
+    <nav className="flex flex-row space-x-10">
+      <TooltipProvider>
+        {items.map((item, index) => {
+          const Icon = Icons[item.icon || 'arrowRight'];
+          return (
+            item.href && (
+              <Tooltip key={index}>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={item.disabled ? '/' : item.href}
+                    className={cn(
+                      'flex items-center gap-2 overflow-hidden rounded-md py-2 text-sm font-medium hover:bg-red-950 hover:bg-opacity-40 hover:backdrop-blur-xl hover:text-accent-foreground',
+                      path === item.href ? 'bg-red-950 bg-opacity-40 backdrop-blur-xl' : 'transparent',
+                      item.disabled && 'cursor-not-allowed opacity-80'
+                    )}
+                    onClick={() => {
+                      if (setOpen) setOpen(false);
+                    }}
+                  >
+                    <Icon className={`ml-3 size-5`} />
+
+                    {isMobileNav || (!isMinimized && !isMobileNav) ? (
+                      <span className="mr-2 truncate">{item.title}</span>
+                    ) : (
+                      ''
+                    )}
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent
+                  align="center"
+                  side="right"
+                  sideOffset={8}
+                  className={!isMinimized ? 'hidden' : 'inline-block'}
+                >
+                  {item.title}
+                </TooltipContent>
+              </Tooltip>
+            )
+          );
+        })}
+      </TooltipProvider>
+    </nav>
+  );
+}
