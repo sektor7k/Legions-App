94269e512090d9e37d9638c2c9939a71b741da19
sektor7k <omeraydin2112@gmail.com>
2024-08-13 23:48:58 +0300

    DashboardNavTournament

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -140,3 +140,63 @@ export function DashboardNavHeader({
     </nav>
   );
 }
+
+
+export function DashboardNavTournament({
+  items,
+  setOpen,
+  isMobileNav = false
+}: DashboardNavProps) {
+  const path = usePathname();
+  const { isMinimized } = useSidebar();
+
+  if (!items?.length) {
+    return null;
+  }
+
+  console.log('isActive', isMobileNav, isMinimized);
+
+  return (
+    <nav className="flex flex-row space-x-10">
+      <TooltipProvider>
+        {items.map((item, index) => {
+          const Icon = Icons[item.icon || 'arrowRight'];
+          return (
+            item.href && (
+              <Tooltip key={index}>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={item.disabled ? '/' : item.href}
+                    className={cn(
+                      'flex items-center gap-2 overflow-hidden rounded-sm p-2 text-sm font-semibold px-5 bg-white bg-opacity-20 border-2 border-gray-400 hover:bg-red-900 hover:border-red-800 hover:bg-opacity-40 hover:backdrop-blur-xl hover:text-accent-foreground',
+                      path === item.href ? 'bg-red-900 border-red-800 bg-opacity-40 backdrop-blur-xl' : 'transparent',
+                      item.disabled && 'cursor-not-allowed opacity-80'
+                    )}
+                    onClick={() => {
+                      if (setOpen) setOpen(false);
+                    }}
+                  >
+
+                    {isMobileNav || (!isMinimized && !isMobileNav) ? (
+                      <span >{item.title}</span>
+                    ) : (
+                      ''
+                    )}
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent
+                  align="center"
+                  side="right"
+                  sideOffset={8}
+                  className={!isMinimized ? 'hidden' : 'inline-block'}
+                >
+                  {item.title}
+                </TooltipContent>
+              </Tooltip>
+            )
+          );
+        })}
+      </TooltipProvider>
+    </nav>
+  );
+}
