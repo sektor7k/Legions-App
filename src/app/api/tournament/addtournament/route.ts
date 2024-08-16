ac26e55f2ec76dd3b46c43f64e77816c36c92f5b
sektor7k <omeraydin2112@gmail.com>
2024-08-17 02:12:27 +0300

    add tournament api route

---
+++ src/app/api/tournament/addtournament/route.ts
@@ -0,0 +1,81 @@
+import { connectDB } from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import Tournament from "@/models/Tournament"; // Tournament modelini import edin
+import mongoose from "mongoose";
+
+export async function POST(request: Request) {
+  try {
+    await connectDB(); // Veritabanına bağlan
+
+    const reqBody = await request.json(); // İstek gövdesini al
+    const {
+      thumnail,
+      tname,
+      tdescription,
+      checkin,
+      checkinTime,
+      starts,
+      startsTime,
+      ends,
+      endsTime,
+      teamsize,
+      teamcount,
+      region,
+      bracket,
+      prizePool,
+    } = reqBody;
+
+    // console.log(tname,
+    //     tdescription,
+    //     checkin,
+    //     checkinTime,
+    //     starts,
+    //     startsTime,
+    //     ends,
+    //     endsTime,
+    //     teamsize,
+    //     teamcount,
+    //     region,
+    //     bracket,
+    //     prizePool)
+
+    // Yeni turnuva oluştur
+    const tournament = new Tournament({
+      thumnail,
+      tname,
+      tdescription,
+      checkin,
+      checkinTime,
+      starts,
+      startsTime,
+      ends,
+      endsTime,
+      teamsize,
+      teamcount,
+      region,
+      bracket,
+      prizePool,
+    });
+
+    // Turnuvayı kaydet
+    const savedTournament = await tournament.save();
+
+    return NextResponse.json(
+      {
+        message: "Tournament created successfully",
+        tournament: savedTournament,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return NextResponse.json(
+        { message: error.message },
+        { status: 400 }
+      );
+    } else {
+      console.error("Error during tournament creation:", error);
+      return NextResponse.error();
+    }
+  }
+}
