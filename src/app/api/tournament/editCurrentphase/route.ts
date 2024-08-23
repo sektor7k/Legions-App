13b8091fbd35185aad99c83ea1a06afb96f1b5ac
sektor7k <omeraydin2112@gmail.com>
2024-08-23 06:11:06 +0300

    currentphase api route

---
+++ src/app/api/tournament/editCurrentphase/route.ts
@@ -0,0 +1,29 @@
+import { connectDB } from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import Tournament from "@/models/Tournament";
+
+
+
+
+export async function POST(request: NextRequest) {
+    try {
+        await connectDB()
+
+        const reqBody = await request.json();
+        console.log(reqBody)
+
+
+        const { currentphase } = reqBody
+        const cardId = reqBody.id
+        const card = await Tournament.findByIdAndUpdate(cardId, { $set: {currentphase} }, { new: true });
+
+        return NextResponse.json({
+            message: "successfully",
+            success: true,
+            card
+        })
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
