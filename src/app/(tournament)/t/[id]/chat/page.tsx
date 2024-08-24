78be557ee4fc2f4ce54dfdc29982ae05d07f6a95
sektor7k <omeraydin2112@gmail.com>
2024-08-24 03:17:45 +0300

    ui modified

--- src/app/(tournament)/t/[id]/chat/page.tsx
+++ src/app/(tournament)/t/[id]/chat/page.tsx
@@ -1,9 +1,15 @@
 export default function ChatPage() {
     return(
-        <div className="w-full">
-           <div className="">
-
-           </div>
+        <div className="flex items-center justify-center mt-2 px-4 sm:px-6 lg:px-4 overflow-y-hidden">
+            <div className="w-full max-w-7xl mt-6 mx-4 lg:mx-8">
+                <div className="bg-black p-4 bg-opacity-50 backdrop-blur-md border-gradient rounded-lg min-h-[calc(87vh-2rem)]">
+                    <div className="overflow-y-auto max-h-[calc(85vh-5rem)] p-2">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                            chat
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
