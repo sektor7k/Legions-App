6abeb8b0c53d36acdf5a6ba21723a7feb3ff657e
sektor7k <omeraydin2112@gmail.com>
2024-08-27 18:48:03 +0300

    send invite api route

---
+++ src/app/api/tournament/invite/sendInvite/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { connectDB } from '@/lib/mongodb';
+import Invite from '@/models/Invite';
+
+export async function POST(request: NextRequest) {
+    const reqBody = await request.json();
+    const { teamId, userId, leadId } = reqBody;
+
+    if (!teamId || !userId || !leadId) {
+        return NextResponse.json({ message: 'Team ID, User ID, and Lead ID are required' }, { status: 400 });
+    }
+
+    await connectDB();
+
+    try {
+        // Yeni bir davet oluştur
+        const newInvite = new Invite({
+            teamId,
+            userId,
+            leadId,
+            status: 'pending', // Varsayılan olarak pending
+        });
+
+        // Daveti kaydet
+        await newInvite.save();
+
+        return NextResponse.json({ message: 'Invite sent successfully', invite: newInvite }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Server error', error }, { status: 500 });
+    }
+}
