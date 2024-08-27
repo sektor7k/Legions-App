9a0b1e23d26e6bb7241a74abee04b5fb17583c87
sektor7k <omeraydin2112@gmail.com>
2024-08-27 18:47:40 +0300

    get team api route

---
+++ src/app/api/tournament/getTeam/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { connectDB } from '@/lib/mongodb';
+import Team from '@/models/Team';
+
+export async function POST(request: NextRequest) {
+    const reqBody = await request.json();
+    const { tournamentId } = reqBody; // Gelen istekteki tournamentId'yi alıyoruz
+
+    await connectDB();
+
+    try {
+        // tournamentId'ye göre tüm takımları bul
+        const teams = await Team.find({ tournamentId });
+
+        if (!teams || teams.length === 0) {
+            return NextResponse.json({ message: 'Teams not found' }, { status: 404 });
+        }
+
+        return NextResponse.json(teams, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Server error', error }, { status: 500 });
+    }
+}
