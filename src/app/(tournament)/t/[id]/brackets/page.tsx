90d654deaddcb62af79fdcd1232f93cdeefd3955
sektor7k <omeraydin2112@gmail.com>
2024-08-30 06:31:42 +0300

    create bracket

--- src/app/(tournament)/t/[id]/brackets/page.tsx
+++ src/app/(tournament)/t/[id]/brackets/page.tsx
@@ -1,37 +1,42 @@
 import TCard from "./_components/Cards";
 
+const teamCount = 16;
+
 export default function Page() {
+    // Turları belirlemek için bir fonksiyon yazıyoruz
+    const generateRounds = (teamCount:number) => {
+        const rounds = [];
+        let currentTeamCount = teamCount;
+
+        while (currentTeamCount >= 2) {
+            rounds.push(currentTeamCount);
+            currentTeamCount = Math.floor(currentTeamCount / 2);
+        }
+
+        return rounds;
+    };
+
+    // Turların sayısını dinamik olarak belirliyoruz
+    const rounds = generateRounds(teamCount);
+
     return (
-        <div className="flex  items-center mt-14 pl-10">
+        <div className="flex  items-center mt-10 pl-10 overflow-x-auto">
             <div className="flex flex-row justify-center mr-3">
-                <ol className="flex flex-1 flex-col justify-around mr-20 space-y-2 round">
-                    {/* Round 1 */}
-                    {Array(16).fill(0).map((_, index) => (
-                        <TCard key={index}/>
-                    ))}
-                </ol>
-                <ol className="flex flex-1 flex-col justify-around mr-20 ml-10 round">
-                    {/* Round 2 */}
-                    {Array(8).fill(0).map((_, index) => (
-                        <TCard key={index}/>
-                    ))}
-                </ol>
-                <ol className="flex flex-1 flex-col justify-around mr-20 ml-10 round">
-                    {/* Round 3 */}
-                    {Array(4).fill(0).map((_, index) => (
-                       <TCard key={index}/>
-                    ))}
-                </ol>
-                <ol className="flex flex-1 flex-col justify-around mr-20 ml-10 round">
-                    {/* Round 4 */}
-                    {Array(2).fill(0).map((_, index) => (
-                        <TCard key={index}/>
-                    ))}
-                </ol>
+                {rounds.map((teamsInRound, roundIndex) => (
+                    <ol
+                        key={roundIndex}
+                        className="flex flex-1 flex-col justify-around mr-20 ml-10 round space-y-3"
+                    >
+                        {/* Her tur için dinamik olarak TCard'ları oluşturuyoruz */}
+                        {Array(teamsInRound).fill(0).map((_, index) => (
+                            <TCard key={index} />
+                        ))}
+                    </ol>
+                ))}
                 <ol className="flex flex-1 flex-col justify-around mr-20 ml-10 round round-winner">
-                    {/*kazanan */}
+                    {/* Kazanan */}
                     {Array(1).fill(0).map((_, index) => (
-                        <TCard key={index}/>
+                        <TCard key={index} />
                     ))}
                 </ol>
             </div>
