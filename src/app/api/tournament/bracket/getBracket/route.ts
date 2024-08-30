9b9374ee01c3666eb08ddd4d86434d00ff2c23cd
sektor7k <omeraydin2112@gmail.com>
2024-08-30 06:32:36 +0300

    get barcket api route

---
+++ src/app/api/tournament/bracket/getBracket/route.ts
@@ -0,0 +1,29 @@
+// /api/tournament/bracket/getBracket.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { connectDB } from '@/lib/mongodb';
+import Bracket from '@/models/Bracket';
+import Team from '@/models/Team'; 
+
+export async function POST(request: NextRequest) {
+    const reqBody = await request.json();
+    const { tournamentId } = reqBody;
+
+    await connectDB();
+
+    try {
+        const bracket = await Bracket.findOne({ tournamentId }).populate({
+            path: 'teams.teamId',
+            select: 'teamName teamImage',
+            match: { _id: { $ne: null } }, // Sadece teamId null olmayanları populate et
+        });
+
+        if (!bracket) {
+            return NextResponse.json({ message: 'Bracket not found' }, { status: 404 });
+        }
+
+        return NextResponse.json(bracket, { status: 200 });
+    } catch (error) {
+        console.error('Error fetching bracket:', error);
+        return NextResponse.json({ message: 'Server error', error }, { status: 500 });
+    }
+}
