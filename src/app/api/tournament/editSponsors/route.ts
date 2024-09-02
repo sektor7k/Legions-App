44f9792ebdf8b184b7239d525e5a7a15abf5db62
sektor7k <omeraydin2112@gmail.com>
2024-09-02 04:22:34 +0300

    edit sponsors api route

---
+++ src/app/api/tournament/editSponsors/route.ts
@@ -0,0 +1,30 @@
+
+import { connectDB } from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import Tournament from "@/models/Tournament";
+
+
+
+
+export async function POST(request: NextRequest) {
+    try {
+        await connectDB()
+
+        const reqBody = await request.json();
+        console.log(reqBody)
+
+
+        const { sponsors } = reqBody
+        const cardId = reqBody.id
+        const card = await Tournament.findByIdAndUpdate(cardId, { $set: {sponsors} }, { new: true });
+
+        return NextResponse.json({
+            message: "successfully",
+            success: true,
+            card
+        })
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
