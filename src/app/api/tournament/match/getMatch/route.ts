944b91b7a3d9e30289a34a6f2925e4838c735078
sektor7k <omeraydin2112@gmail.com>
2024-11-09 01:57:30 +0300

    get match api

---
+++ src/app/api/tournament/match/getMatch/route.ts
@@ -0,0 +1,39 @@
+import { connectDB } from "@/lib/mongodb";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+import Match from "@/models/Matches"; // Matches modelini import edin
+
+export async function POST(request: Request) {
+    try {
+        await connectDB();
+
+        const reqBody = await request.json();
+        const { tournamentId } = reqBody;
+
+        const matches = await Match.find({ tournamentId })
+            .populate({
+                path: "team1Id",
+                select: "teamName teamImage",
+            })
+            .populate({
+                path: "team2Id",
+                select: "teamName teamImage",
+            });
+
+        return NextResponse.json(
+            { message: "getMatches successfully", data: matches },
+            { status: 200 }
+        );
+
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return NextResponse.json(
+                { message: error.message },
+                { status: 400 }
+            );
+        } else {
+            console.error("Error during match fetching:", error);
+            return NextResponse.error();
+        }
+    }
+}
