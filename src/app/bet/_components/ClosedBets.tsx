4722e7595f86b72342d55d4a0e40f15c880004e4
sektor7k <omeraydin2112@gmail.com>
2024-11-16 12:11:23 +0300

    closed bets ui

---
+++ src/app/bet/_components/ClosedBets.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import { FaForward } from "react-icons/fa";
+
+export default function ClosedBets(){
+    return(
+        <div className="w-full flex flex-col space-y-4">
+                <div className="flex flex-row bg-gray-900 h-12 rounded-md">
+                    Closed Bets
+
+                </div>
+                
+            </div>
+    )
+}
