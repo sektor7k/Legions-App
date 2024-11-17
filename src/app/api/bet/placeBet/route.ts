9c8e4a90f0854977b4a3590249945f1f60b1f7aa
sektor7k <omeraydin2112@gmail.com>
2024-11-18 02:18:23 +0300

    place bet api route

---
+++ src/app/api/bet/placeBet/route.ts
@@ -0,0 +1,51 @@
+import { connectDB } from "@/lib/mongodb";
+import Bet from "@/models/Bet";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+
+export async function POST(request: NextRequest) {
+    try {
+        await connectDB();
+
+        const session = await getServerSession(authOptions);
+
+        if (!session) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+          }
+
+        const reqBody = await request.json();
+        const { tournamentId, matchId, selectedTeamId, stake } = reqBody;
+
+        if (!tournamentId || !matchId || !selectedTeamId || !stake) {
+            return NextResponse.json({ message: "All fields are required" }, { status: 400 });
+          }
+
+          const founderId = session.user.id
+
+        const bet = new Bet({
+            founderId,
+            tournamentId,
+            matchId,
+            selectedTeamId,
+            stake
+        })
+
+        const savedBet = await bet.save();
+
+        return NextResponse.json({ data: savedBet }, { status: 201 })
+
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return NextResponse.json(
+                { message: error.message },
+                { status: 400 }
+            );
+        } else {
+            console.error("Error during match creation:", error);
+            return NextResponse.error();
+        }
+    }
+}
