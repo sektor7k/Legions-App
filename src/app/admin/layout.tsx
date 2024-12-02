8c95a04a9ecae54008848863a100c720826308e4
sektor7k <omeraydin2112@gmail.com>
2024-12-02 17:10:06 +0300

    sidebar add admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,19 +1,27 @@
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "./_components/app-sidebar";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
 
     return (
-        <div  >
-            <div className="relative flex h-screen overflow-hidden">
-                <main className="flex-1 pt-14 pb-14 relative overflow-y-auto ">
-                    <div className="fixed inset-0 bg-bg-auth bg-cover bg-center h-full"></div>
-                    <div className="relative z-10">
-                        {children}
-                    </div>
-                </main>
+        <SidebarProvider>
+
+            <div  >
+                <div className="relative flex h-screen overflow-hidden">
+                    <AppSidebar />
+                    <main className="flex-1 pb-14 relative overflow-y-auto ">
+
+                        <div className="fixed inset-0 bg-bg-auth bg-cover bg-center h-full"></div>
+                        <div className="relative z-auto">
+
+                            <SidebarTrigger />
+                            {children}
+                        </div>
+                    </main>
+                </div>
             </div>
-        </div>
-    );  
+        </SidebarProvider>
+    );
 };
 
 export default AdminLayout;
