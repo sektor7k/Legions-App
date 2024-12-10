14c9961001061508f014a50b36ff4154a9c428fe
sektor7k <omeraydin2112@gmail.com>
2024-12-10 05:20:45 +0300

    set activity stream to setwinner api

--- src/app/api/bet/setWinner/route.ts
+++ src/app/api/bet/setWinner/route.ts
@@ -1,19 +1,40 @@
 import { connectDB } from "@/lib/mongodb";
 import Bet from "@/models/Bet";
+import User from "@/models/User";
+import axios from "axios";
 import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function POST(request: NextRequest) {
-    try {
+    try { 
 
         await connectDB();
 
         const reqBody = await request.json();
-        const { winnerId, betId } = reqBody;
+        const { winnerId, betId, stake } = reqBody;
 
 
         const setWinner = await Bet.findByIdAndUpdate(betId, { winnerId, status: "completed" });
+
+        const winnerUser = await User.findById(winnerId, 'username image');
+
+        console.log(winnerUser)
+
+        if (!winnerUser) {
+            return NextResponse.json({ message: "Winner user not found" }, { status: 404 });
+        }
+
+        try {
+            await axios.post('/api/bet/stream', {
+                status: 'won',
+                amount: stake,
+                username: winnerUser.username,
+                userAvatar: winnerUser.image 
+            });
+        } catch (error) {
+            console.error("Error adding to activity feed:", error);
+        }
 
 
         return NextResponse.json({ data: null }, { status: 201 })
