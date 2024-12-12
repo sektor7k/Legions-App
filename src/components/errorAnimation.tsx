9a5a7a03cd33b9aca10feb16162eafabd22538cb
sektor7k <omeraydin2112@gmail.com>
2024-12-12 04:11:33 +0300

    error animation

---
+++ src/components/errorAnimation.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React, { useEffect, useState } from 'react';
+import Lottie from 'react-lottie';
+
+export default function ErrorAnimation() {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch('/error.json')
+      .then(response => response.json()) 
+      .then(data => setAnimationData(data));
+  }, []);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    },
+    isClickToPauseDisabled: true // Kullanıcı tıklamalarını devre dışı bırak
+  };
+
+  return (
+    <div style={{ pointerEvents: 'none' }}>
+      {animationData && <Lottie options={defaultOptions} height={200} width={200} />}
+    </div>
+  );
+}
